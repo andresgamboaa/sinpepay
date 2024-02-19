dbc48a55e1314f95302727ccf878619ec75c8eac
andresgamboaa <andres24k8@gmail.com>
2024-02-19 14:45:59 -0600

    Add banks

--- src/banks.ts
+++ src/banks.ts
@@ -7,4 +7,18 @@ export interface Bank {
 export const banks: Bank[] = [
     { name: "BN", number: 2627 },
     { name: "BCR", number: 4066 },
+    { name: "BAC", number: 70701222},
+    { name: "BCT", number: 60400300},
+    { name: "Caja de Ande", number: 62229532},
+    { name: "Coopealianza", number: 62229523},
+    { name: "Coocique", number: 62229537},
+    { name: "Coopecaja", number: 62229526},
+    { name: "Coopelecheros", number: 60405957},
+    { name: "Coopenae", number: 62232599},
+    { name: "Davivienda", number: 70707474},
+    { name: "LAFIASE", number: 9091},
+    { name: "MUCAP", number: 62229525},
+    { name: "Mutual Alajuela", number: 70707079},
+    { name: "Promerica", number: 62232450},
+    { name: "Credecoop", number: 71984256},
 ]
