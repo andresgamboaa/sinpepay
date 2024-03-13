53f32c977f84d50ee135ec7b07e43932a2abd1ac
andresgamboaa <andres24k8@gmail.com>
2024-03-13 08:52:09 -0600

    Center crear enlace

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,14 +11,18 @@ export default function Navbar({onBankChanged}: Props) {
 
     return (
         <div class="w-full h-fit flex flex-col gap-4 md:flex-row items-center justify-center md:justify-normal p-4 mx-auto max-w-[1200px]">
-            <a href="/" class=" text-2xl font-bold"><span class="text-black">sinpe</span><span class=" text-orange-600">pay</span></a>
-            <div class="w-full flex text-black justify-center ">
+            <div class="flex-1">
+                <a href="/" class="text-2xl font-bold"><span class="text-black">sinpe</span><span class=" text-orange-600">pay</span></a>
+            </div>
+            <div class="flex-1 flex text-black justify-center ">
                 <button onClick={() => setShowCreateLinkModal(true)}>Crear enlace</button>
             </div>
-            <SelectBank 
-                onBankChanged={onBankChanged}
-            />
-            {showCreateLinkModal && <CreateLinkModal onClose={() => setShowCreateLinkModal(false)}/>}
+            <div class="flex-1">
+                <SelectBank 
+                    onBankChanged={onBankChanged}
+                />
+            </div>
+           {showCreateLinkModal && <CreateLinkModal onClose={() => setShowCreateLinkModal(false)}/>}
         </div>
     )
 }
